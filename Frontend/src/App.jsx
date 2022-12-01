ab34f525d536622b451d9f3695e780179a566aa4
Dhanushk19 <95034453+Dhanushk19@users.noreply.github.com>
2022-12-01 14:05:13 +0530

    FrontendSlotCreation

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,7 +6,7 @@ import {BrowserRouter ,Routes,Route} from 'react-router-dom';
 import Slot from './components/CreateSlot';
 import Home from './components/Home';
 import About from './components/About';
-
+import SlotBook from "./components/SlotBooking";
 function App() {
   return (
     <div className="App"> 
@@ -16,6 +16,7 @@ function App() {
       <Route path="/home" element={<Home/>} /> 
       <Route path="/slot" element={<Slot/>}/>
       <Route path='/about' element={<About/>}/>
+      <Route path="/slotbook" element={<SlotBook/>}/>
     </Routes>
     </BrowserRouter>
     </div>
