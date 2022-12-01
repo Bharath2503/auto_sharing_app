8afb3cc529707248e40bc470294b2d01b5a9fdbd
Dhanushk19 <95034453+Dhanushk19@users.noreply.github.com>
2022-12-01 12:41:33 +0530

    Home page Front end

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,9 +1,31 @@
+import "../css/Home.css";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 function Home()
 {
     return(
-        <div>
-            <h1>Home</h1>
+        
+        <div className="card mx-auto" style={{width:"60%",backgroundColor:"#058240"}}>
+        <div className="card-body">
+          <h4 className="card-title" style={{textAlign:"center",color:"white",paddingBottom:"5%",paddingTop:"3%"}}>Login</h4>
+          <Form>
+      <Form.Group className="mb-3 form-group" controlId="formBasicEmail">
+        <Form.Control type="email" placeholder="Email" className="form-control" />
+      </Form.Group>
+      <Form.Group className="mb-3 form-group"  controlId="formBasicPassword">
+        <Form.Control type="password" placeholder="Password" className="form-control"/>
+      </Form.Group>
+     <center>
+     <Button variant="light" type="submit" className="form-button" >
+        Login
+      </Button>
+     </center>
+    </Form>
+          
+         
         </div>
+      </div>
     )
 }
 export default Home;
