7feffee136dc095ad91eca511acb9309df971f1b
Dhanushk19 <95034453+Dhanushk19@users.noreply.github.com>
2022-12-17 15:32:03 +0530

    SlotDetails page modification

--- Frontend/src/components/SlotDetails.jsx
+++ Frontend/src/components/SlotDetails.jsx
@@ -1,19 +1,19 @@
 import Button from 'react-bootstrap/Button';
 import "../css/slotdetails.css";
 import { Link } from 'react-router-dom';
+import 'font-awesome/css/font-awesome.min.css';
 function SlotDetails() {
     return (
         <div className='SlotDetailsPage'>
             <div className='SlotPage-Box'>
             <h4 className="SlotDetails-Title">Slot details</h4>
-            <h6 className="SlotDetails-owner"><b>Created by:</b></h6>
+            <h5 className="SlotDetails-owner">Created by:</h5>
+            <br></br>
             <div className="SlotMembers">
-                <h5 className="SlotMembers-Title">Slot Members</h5>
+                <h6 className="SlotMembers-Title">Slot Members</h6>
                 <div className='Members-Email'>
-                <ul>
-                    <li></li>
-                </ul>
-            </div>
+                <p className='Slotdetails-List'><i className="fa fa-user" style={{fontSize:17}}>&ensp;</i>aathav20it@</p>
+                </div>
             </div>
             <h5 className='SlotMembers-Number'>( /4)</h5>
           <Link to="/SlotBook"><Button type="submit" variant='danger' className='SlotLeaveButton'>Leave</Button></Link>  
